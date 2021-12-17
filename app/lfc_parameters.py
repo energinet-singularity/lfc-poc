c50658c13f66922e8dc2913a28f52a00c51a2cac
Anders Wittendorff <andersw89@gmail.com>
2021-12-17 10:08:09 +0100

    Commit of first LFC code
    initial version

---
+++ app/lfc_parameters.py
@@ -0,0 +1,25 @@
+# settings for LFC
+
+# Timouts
+TIMEOUT_MS_POLL = 100 # Kafka poll
+
+# refresh rates
+REFRESH_RATE_MS_LFC_INPUT = 100
+REFRESH_RATE_S_LFC_DEM_SIMU = 1
+
+# decimal precision
+PRECISION_DECIMALS = 2
+
+# lfc cycle time
+CYCLETIME_S_LFC_CONT = 4
+
+# LFC controller settings (PID, deadband for controller to force error to zero)
+KP = 0.008
+KI = 0.014
+KD = 0
+DEADBAND_LFC_ERROR = 0.5
+
+# pbr responce simu settings
+PBR_RAMP_MWM = 30
+PBR_RAMP_MWS = PBR_RAMP_MWM/60
+DEADBAND_PBR_SIMU = PBR_RAMP_MWS*REFRESH_RATE_S_LFC_DEM_SIMU
