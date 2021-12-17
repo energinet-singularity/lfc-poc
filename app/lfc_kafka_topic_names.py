669d0574d7ee014f2e06da96ba537249bd4955a0
Anders Wittendorff <94957526+CodeWitten@users.noreply.github.com>
2021-12-17 13:27:02 +0100

    Test/firsttime (#1)
    * Commit of first LFC code
    
    initial version
    
    * added kafka helper functions
    
    * modified comment

---
+++ app/lfc_kafka_topic_names.py
@@ -0,0 +1,19 @@
+# topics names for LFC 
+
+# Source measurements for LFC input calc
+lfc_p_corr = 'lfc-p.correction.value'
+lfc_p_dem = 'lfc-p.demand.value'
+
+# Calculated input for LFC and state of input calc
+lfc_p_input = 'lfc-p.input.value'
+lfc_p_input_calc_state = 'lfc-p.input.calc.state'
+
+# target from LFC controller and PID controller states
+lfc_p_target = 'lfc-p.target.value'
+lfc_p_target_state = 'lfc-p.target.calc.state'
+
+# disturbance im mw
+lfc_mw_diff = 'lfc-mw.diff.value'
+
+# pbr response simulated value
+lfc_pbr_response = 'lfc-pbr.response.value'
